00c43a749ed0c5ef1c91de1bbbec1c2664a9bc5e
Prabhav Manas <prabhavmanas17@gmail.com>
2026-05-07 16:40:36 +0530

    connect to database

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const cors=require('cors');
+const mongoose=require('mongoose');
 
 const app=express();
 
@@ -21,5 +22,11 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log('Connected to DB!');
+}).catch((error)=>{
+    console.log('Faild to connect to DB!');
+})
 
 module.exports=app;
