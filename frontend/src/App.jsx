6f8d091a9fa6db7a74d522e367322aad540ece53
Prabhav Manas <prabhavmanas17@gmail.com>
2026-05-07 17:24:15 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return(
+    <div className="container">
+      <div className="row">
+        <h1 className='text-center text-success'>App</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home=()=>{
+    return(
+        <h2 className="">Home Page</h2>
+    )
+}
+
+export default Home
