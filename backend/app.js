e78b36e58fb638b4a8a146da89596156a4839fa4
Prabhav Manas <prabhavmanas17@gmail.com>
2026-05-08 16:40:44 +0530

    modify with live url

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const scrapeRoutes=require("./src/routes/scrapeRoutes");
 const app=express();
 
 const corsOptions={
-    origin:['http://localhost:5173', process.env.FRONTEND_URL],
+    origin:['http://localhost:5173', 'https://stirring-malabi-0ccc9d.netlify.app', process.env.FRONTEND_URL],
     methods:"GET,POST,HEAD,PUT,PATCH,DELETE,OPTIONS",
     allowedHeaders:[
         "Content-Type",
